2a27171db4f9a7105936fd1c993a61d21231f06e
MR-Addict <2750417853@qq.com>
2023-03-22 21:09:30 +0800

    polish football page

--- src/app/football/page.tsx
+++ src/app/football/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import style from "./page.module.css";
+
+import { setMetadata } from "@/lib/utils";
+import background from "@/data/assets/footballbg.png";
+
+export const metadata = setMetadata("足球仿真");
+
 export default function Page() {
   return (
-    <main className='w-full flex-1 frame background flex flex-col items-center justify-center gap-5'>
-      <h1>Hello world</h1>
+    <main className='background w-full flex-1 frame flex flex-col items-center justify-center gap-5'>
+      <Image placeholder='blur' src={background} alt='background' className='w-full max-w-sm' />
+      <div className='flex flex-col md:flex-row gap-5 md:gap-7'>
+        <Link href='/lego/signup' className={style.button}>
+          参与报名
+        </Link>
+        <Link href='/lego/infomation' className={style.button}>
+          报名信息
+        </Link>
+      </div>
     </main>
   );
 }
--- src/app/lego/page.tsx
+++ src/app/lego/page.tsx
@@ -10,7 +10,7 @@ export const metadata = setMetadata("创意乐高");
 export default async function Page() {
   return (
     <main className='background w-full flex-1 frame flex flex-col items-center justify-center gap-5'>
-      <Image placeholder='blur' src={background} alt='background' className='w-full max-w-xs' />
+      <Image placeholder='blur' src={background} alt='background' className='w-full max-w-md' />
       <div className='flex flex-col md:flex-row gap-5 md:gap-7'>
         <Link href='/lego/signup' className={style.button}>
           参与报名
